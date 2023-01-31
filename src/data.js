cb160ce93b70b21143176eda4210349fd87dd9dd
gbalasubramani <gbalasubramani@costar.com>
2023-01-30 22:22:53 -0800

    updated the bbs

--- src/data.js
+++ src/data.js
@@ -1,54 +1,54 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Search and result page",
+    subtitle: "Angular and Material UI",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "The search bar along with the detail shown below are developed in angular material with home grown scss.The RxJS powered state management for the aplication was implemented in this page.",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.loopnet.com/biz/businesses-for-sale/",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Filter Bar for BBS",
+    subtitle: "Angular Material",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Designed the filter bar for this page using typescript,html and scss.Did seo optimisation for this page to increase the traffic.Converted this application to a module based application.",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.bizbuysell.com/california-businesses-for-sale/?q=bHQ9MzAsNDAsODA%3D",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "Detail page for bizquest",
+    subtitle: "Angular Reactive forms",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Detail page has REST api integration along with a reactive form and required validations",
     image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.bizquest.com/the-learning-experience-franchise-for-sale/",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Photo upload and thumbnail view",
+    subtitle: "Angular cropping and drag & drop",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Designed a photo uplodaing tool that allows to crop the images as per users needs",
     image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.bizbuysell.com/fsbo/listings/add/Basic.aspx",
   },
 ];
 
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
-];
+// export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+// ];
 
 export const skills = [
   "Angular",
